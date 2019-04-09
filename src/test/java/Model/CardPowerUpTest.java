344dd90a390a36e934e78c1d1c4465c6afe6f744
Marco <MarcoPianta@users.noreply.github.com>
2019-04-09 12:43:22 +0200

    Add powerUp test

--- src/test/java/Model/CardPowerUpTest.java
+++ src/test/java/Model/CardPowerUpTest.java
@@ -1,31 +1,26 @@
 package Model;
 
 import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CardPowerUpTest {
+
     @Test
-    public void testConstructor(){
-        CardPowerUp cardPowerUp;
+    public void testConstructor() throws FileNotFoundException {
+        CardPowerUp cardPowerUp = new CardPowerUp(PowerUpEnum.NEWTON_B.getAbbreviation());
 
-        cardPowerUp = new CardPowerUp();
-        assertTrue(cardPowerUp instanceof CardPowerUp);
-    }
-    @Test
-    public void testgetBonusEffect() {
-        CardPowerUp cardPowerUp;
-        cardPowerUp = new CardPowerUp();
-
-        assertNull(cardPowerUp.getBonusEffect());
-    }
-    @Test
-    public void testgetString(){
-        CardPowerUp cardPowerUp;
-        cardPowerUp = new CardPowerUp();
-
-        assertNull(cardPowerUp.getName());
-
+        assertEquals(cardPowerUp.getName()+"_B", PowerUpEnum.NEWTON_B.getAbbreviation());
+        assertEquals(cardPowerUp.getColor(), AmmoColors.BLUE);
+        assertEquals(0, cardPowerUp.getCost());
+        assertEquals(0, cardPowerUp.getDamage());
+        assertEquals(0, cardPowerUp.getMark());
+        assertEquals(0, cardPowerUp.getMyMove());
+        assertEquals(2, cardPowerUp.getOtherMove());
+        assertEquals(1, cardPowerUp.getTarget());
+        assertEquals("during", cardPowerUp.getWhen());
+        assertFalse(cardPowerUp.getVision());
     }
 }
-
-
