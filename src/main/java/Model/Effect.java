1606a94edc85512782df2d83c0bc92398dd5e55f
Marco <MarcoPianta@users.noreply.github.com>
2019-04-20 12:04:42 +0200

    Added pre and post conditions to Effect Class

--- src/main/java/Model/Effect.java
+++ src/main/java/Model/Effect.java
@@ -2,6 +2,9 @@ package Model;
 
 import javax.json.JsonObject;
 
+/**
+ * This Class contains the possible effect of a CardWeapon
+ * */
 public class Effect {
     private int targetNumber;
     private boolean allTarget;
@@ -10,6 +13,8 @@ public class Effect {
     private int myMove;
     private int otherMove;
     private int iteration;
+    private PreCondition preCondition;
+    private PostCondition postCondition;
 
     public Effect(JsonObject jsonValues, int effect){
         String jsonString = "Effect" + effect;
@@ -20,6 +25,8 @@ public class Effect {
         myMove = jsonValues.getJsonArray(jsonString).getJsonObject(0).getInt("myMove");
         otherMove = jsonValues.getJsonArray(jsonString).getJsonObject(0).getInt("otherMove");
         iteration = jsonValues.getJsonArray(jsonString).getJsonObject(0).getInt("iteration");
+        preCondition = new PreCondition(jsonValues.getJsonArray(jsonString).getJsonObject(0).getJsonArray("preCondition").getJsonObject(0));
+        postCondition = new PostCondition(jsonValues.getJsonArray(jsonString).getJsonObject(0).getJsonArray("postCondition").getJsonObject(0));
     }
 
     public int getTargetNumber() {
@@ -48,5 +55,85 @@ public class Effect {
 
     public int getIteration() {
         return iteration;
+    }
+
+    public PreCondition getPreCondition() {
+        return new PreCondition(preCondition);
+    }
+
+    public PostCondition getPostCondition() {
+        return new PostCondition(postCondition);
+    }
+
+    /**
+     * This inner class is used to contains the pre condition read from Weapon json file
+     * */
+    class PreCondition{
+        private boolean vision;
+        private boolean enemiesDifferentSquare;
+        private int minRange;
+        private int maxRange;
+        private boolean cardinal;
+
+        public PreCondition(JsonObject jsonValues){
+            vision = jsonValues.getBoolean("vision");
+            enemiesDifferentSquare = jsonValues.getBoolean("enemiesDifferentSquare");
+            minRange = jsonValues.getInt("minRange");
+            maxRange = jsonValues.getInt("maxRange");
+            cardinal = jsonValues.getBoolean("cardinal");
+        }
+
+        /**
+         * This constructor can be used to create copy of a PreCondition object
+         * */
+        public PreCondition(PreCondition preCondition){
+            this.vision = preCondition.isVision();
+            this.enemiesDifferentSquare = preCondition.isEnemiesDifferentSquare();
+            this.minRange = preCondition.getMinRange();
+            this.maxRange = preCondition.getMaxRange();
+            this.cardinal = preCondition.isCardinal();
+        }
+
+        public boolean isVision() {
+            return vision;
+        }
+
+        public boolean isCardinal() {
+            return cardinal;
+        }
+
+        public boolean isEnemiesDifferentSquare() {
+            return enemiesDifferentSquare;
+        }
+
+        public int getMaxRange() {
+            return maxRange;
+        }
+
+        public int getMinRange() {
+            return minRange;
+        }
+    }
+
+    /**
+     * This inner class is used to contains the post condition read from Weapon json file
+     * */
+    class PostCondition{
+        private int targetMove;
+
+        public PostCondition(JsonObject jsonValues){
+            targetMove = jsonValues.getInt("targetMove");
+        }
+
+        /**
+         * This constructor can be used to create copy of a PostCondition object
+         * */
+        public PostCondition(PostCondition postCondition){
+            this.targetMove = postCondition.getTargetMove();
+        }
+
+        public int getTargetMove() {
+            return targetMove;
+        }
     }
 }
