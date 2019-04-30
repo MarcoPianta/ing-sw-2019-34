872eea0b4f66ae566fc8a8bb26894ee93aa09d0f
Marco <MarcoPianta@users.noreply.github.com>
2019-04-30 11:18:13 +0200

    Added CardAmmos tests

---
+++ src/test/java/Model/CardNotOnlyAmmoTest.java
@@ -0,0 +1,40 @@
+package Model;
+
+import org.junit.jupiter.api.Test;
+import java.io.FileNotFoundException;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CardNotOnlyAmmoTest {
+
+    /**
+     * This method tests if CardNotOnlyAmmo read form json file and initialize attributes correctly.
+     * */
+    @Test
+    public void constructorTest() throws FileNotFoundException {
+        CardNotOnlyAmmo cardNotOnlyAmmo;
+        Deck<CardPowerUp> deck, deckCopy;
+        DeckCreator deckCreator = new DeckCreator();
+        Drawer<CardPowerUp> drawer;
+
+        cardNotOnlyAmmo = new CardNotOnlyAmmo(AmmoEnum.AMMO1.getAbbreviation());
+        deck = deckCreator.createDeck("POWERUP");
+        drawer = deck.createDrawer();
+        deckCopy = deck.clone();
+
+        assertEquals(AmmoColors.BLUE, cardNotOnlyAmmo.getItem2());
+        assertEquals(AmmoColors.BLUE, cardNotOnlyAmmo.getItem3());
+        cardNotOnlyAmmo.getItem1(drawer);
+        assertEquals(deck.getSize(), deckCopy.getSize()-1);
+    }
+
+    @Test
+    public void copyCardTest() throws FileNotFoundException{
+        CardNotOnlyAmmo cardNotOnlyAmmo, cardNotOnlyAmmoCopy;
+
+        cardNotOnlyAmmo = new CardNotOnlyAmmo(AmmoEnum.AMMO1.getAbbreviation());
+        cardNotOnlyAmmoCopy = cardNotOnlyAmmo.copyCardAmmo();
+
+        assertEquals(cardNotOnlyAmmo.getItem2(), cardNotOnlyAmmo.getItem2());
+        assertEquals(cardNotOnlyAmmo.getItem3(), cardNotOnlyAmmo.getItem3());
+    }
+}
---
+++ src/test/java/Model/CardOnlyAmmoTest.java
@@ -0,0 +1,39 @@
+package Model;
+
+import org.junit.jupiter.api.Test;
+import java.io.FileNotFoundException;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CardOnlyAmmoTest {
+
+    /**
+     * This method tests if the constructor read from json file and create the card correctly
+     * */
+    @Test
+    public void constructorsTest() throws FileNotFoundException {
+        CardOnlyAmmo cardOnlyAmmo;
+        cardOnlyAmmo = new CardOnlyAmmo(AmmoEnum.AMMO1.getAbbreviation());
+
+        assertEquals(AmmoColors.YELLOW, cardOnlyAmmo.getItem1());
+        assertEquals(AmmoColors.BLUE, cardOnlyAmmo.getItem2());
+        assertEquals(AmmoColors.BLUE, cardOnlyAmmo.getItem3());
+
+        //Test of default constructor only to get 100% coverage
+        cardOnlyAmmo = new CardOnlyAmmo();
+
+        assertNull(cardOnlyAmmo.getItem1());
+    }
+
+    @Test
+    public void copyCardAmmoTest() throws FileNotFoundException{
+        CardOnlyAmmo cardOnlyAmmo;
+        CardOnlyAmmo copy;
+
+        cardOnlyAmmo = new CardOnlyAmmo(AmmoEnum.AMMO1.getAbbreviation());
+        copy = cardOnlyAmmo.copyCardAmmo();
+
+        assertEquals(cardOnlyAmmo.getItem1(), copy.getItem1());
+        assertEquals(cardOnlyAmmo.getItem2(), copy.getItem2());
+        assertEquals(cardOnlyAmmo.getItem3(), copy.getItem3());
+    }
+}
