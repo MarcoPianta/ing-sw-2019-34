346d5a126696eca61689d0444282cedf0af86fa7
Marco <MarcoPianta@users.noreply.github.com>
2019-05-05 19:11:24 +0200

    Added some code to Socket package, implementation not complete

--- src/main/java/network/Server/Socket/SocketClient.java
+++ src/main/java/network/Server/Socket/SocketClient.java
@@ -1,10 +1,60 @@
 package network.Server.Socket;
 
+import network.messages.Message;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
+/**
+ * This class is used from the client to communicate with SocketServer when a Socket connection is used.
+ * */
 public class SocketClient {
-    private Socket socket;
+    private String host;
+    private final int port;
+    private Socket connection;
     private SocketServer server;
+    private ObjectInputStream in; //Used to send message to server
+    private ObjectOutputStream out; ////Used to receive message from server
 
+    /**
+     * The constructor only initialize the host and port attribute, it doesn't establish connection. Attributes will be
+     * used from init() to establish a new connection to the server
+     * */
+    public SocketClient(String host, int port){
+        this.host = host;
+        this.port = port;
+    }
 
+    /**
+     * This method initialize a new socket connection and set the output and input stream used to send and receive
+     * message from the server.
+     * */
+    public void init() throws IOException {
+        connection = new Socket(host, port);
+        out = new ObjectOutputStream(connection.getOutputStream());
+        in = new ObjectInputStream(connection.getInputStream());
+    }
+
+    /**
+     * This method send a message to the server
+     * @param message is the message that must be sent to the server
+     * */
+    //TODO handle exception, maybe with a logger
+    public void send(Message message) throws IOException{
+        out.writeObject(message);
+    }
+
+    //TODO receive response message
+
+    /**
+     * This method is used to close connection between the client and the server. Before closing connection input and
+     * output streams are closed.
+     * */
+    public void close() throws IOException {
+        in.close();
+        out.close();
+        connection.close();
+    }
 }
--- src/main/java/network/Server/Socket/SocketServer.java
+++ src/main/java/network/Server/Socket/SocketServer.java
@@ -1,23 +1,64 @@
 package network.Server.Socket;
 
-import network.Server.Client;
+import network.Server.Server;
+import network.messages.ActionType;
+import network.messages.Message;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 
 public class SocketServer {
+    private ServerSocket serverSocket;
     private int port;
-    private ServerSocket sc;
-    private ArrayList<Client> clients;
+    private Server server; //This variable is used to contain the main server
+    private ArrayList<Socket> clients;
 
-    public SocketServer() throws IOException {
-        sc = new ServerSocket(10000);
+    public SocketServer(Server server, int port) {
+        this.server = server;
+        this.port = port;
         clients = new ArrayList<>();
     }
 
-    public void run() throws IOException{
-        clients.add(new Client(sc.accept()));
+    //TODO catch exception
+    private void init() throws IOException{
+        serverSocket = new ServerSocket(port);
+    }
 
+    //TODO catch exception
+    public Socket acceptConnection() throws IOException{
+        Socket socket = serverSocket.accept();;
+        clients.add(socket);
+        return socket;
+    }
+
+    //TODO catch exception
+    public void handleConnection(Socket client) throws IOException{
+
+        try {
+            ObjectOutputStream out = new ObjectOutputStream(client.getOutputStream());
+            ObjectInputStream in = new ObjectInputStream(new BufferedInputStream(client.getInputStream()));
+
+            Message m = (Message) in.readObject();
+
+
+        }catch (IOException e){
+            //TODO handle exception, maybe with a logger
+        }catch (ClassNotFoundException e){
+            //TODO handle exception, maybe with a logger
+        }
+    }
+
+    public void readMessage(Message message){
+        if (message.getActionType().getAbbreviation().equals(ActionType.CONNECTIONREQUEST.getAbbreviation()) ){
+
+        }
+    }
+
+    public void run() throws IOException{
+        init();
+
+        acceptConnection();
     }
 }
