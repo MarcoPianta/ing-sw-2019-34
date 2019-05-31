ac75eb6bc3e186c3c534fe1c598b75405c972687
Marco <MarcoPianta@users.noreply.github.com>
2019-05-31 16:40:11 +0200

    Added code to View abstract class, added code to UpdateClient

--- src/main/java/network/messages/UpdateClient.java
+++ src/main/java/network/messages/UpdateClient.java
@@ -11,6 +11,7 @@ public class UpdateClient extends Message {
     private List<Colors> damageBar; //The damageBar is made of colors to be displayed
     private String squareID; //The id of the square to send in the message
     private ArrayList<String> reachableSquares; //ArrayList of IDs of squares to be send
+    private ArrayList<String> reachableTarget; //ArrayList of possible target Players' square ID
     private String type; //Type of update (position, damageBar, etc.)
     private String message;
 
@@ -26,7 +27,7 @@ public class UpdateClient extends Message {
      * @param damageBar the array which contains the new damageBar to send to the client
      */
     public UpdateClient(Integer token, Player[] damageBar){
-        setMessageInfo(token, DAMAGEBAR);
+        setMessageInfo(token, DAMAGEBARUPDATE);
         this.damageBar = new ArrayList<>();
         for (Player p: damageBar){
             this.damageBar.add(p.getColor());
@@ -50,6 +51,7 @@ public class UpdateClient extends Message {
      */
     public UpdateClient(Integer token, List<NormalSquare> squares){
         setMessageInfo(token, POSSIBLESQUARES);
+        this.reachableSquares = new ArrayList<>();
         for (NormalSquare s: squares)
             this.reachableSquares.add(s.getId());
     }
@@ -61,8 +63,9 @@ public class UpdateClient extends Message {
      */
     public UpdateClient(Integer token, ArrayList<Player> players){
         setMessageInfo(token, POSSIBLESQUARES);
+        this.reachableTarget = new ArrayList<>();
         for (Player p: players)
-            this.reachableSquares.add(p.getPosition().getId());
+            this.reachableTarget.add(p.getPosition().getId());
     }
 
     /**
@@ -82,15 +85,23 @@ public class UpdateClient extends Message {
     /**
      * The following static values are used to distinguish the UpdateClient message type.
      */
-    public static final String DAMAGEBAR ="DAMAGEBAR";
-    public static final String POSITION = "POSITION";
-    public static final String POSSIBLESQUARES = "POSSIBLESQUARES";
     public static final String POSSIBLETARGET = "POSSIBLETARGET";
+    public static final String DAMAGEBARUPDATE ="DAMAGEBAR";
+    public static final String POSSIBLESQUARES = "POSSIBLESQUARES";
+    public static final String POSITION = "POSITION";
     public static final String MESSAGE = "MESSAGE";
 
 
+    public ArrayList<String> getReachableTarget() {
+        return reachableTarget;
+    }
+
     public List<Colors> getDamageBar() {
         return damageBar;
+    }
+
+    public ArrayList<String> getReachableSquares() {
+        return reachableSquares;
     }
 
     public String getSquareID() {
--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,18 +1,91 @@
 package view;
 
-import Model.Colors;
+import Model.*;
+import network.Server.Client;
+import network.messages.GameSettingsResponse;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
  * This class contains the data and method used from the cli or the GUI to show the current state of the game.
  */
 public abstract class View {
-    public abstract void showReachableSquares();
+    protected Client client;
+
+    protected ArrayList<CardWeapon> weapons; /**The weapons owned from hte player*/
+    protected int blueAmmo; /**Number of blue ammo*/
+    protected int redAmmo; /**Number of red ammo*/
+    protected int yellowAmmo; /**Number of yellow ammo*/
+    protected ArrayList<Colors> damageBar; /**The player damage bar, is an array*/
+    protected ArrayList<Colors> marks;
+    protected ArrayList<CardPowerUp> powerUps; /**The powerUps owned from the player*/
+    protected GameBoard map;
+    protected int maxReward; /**The max reward gained from other players when player die*/
+    protected String myPositionID; /**This variable contains the ID of player's current square */
+    protected ArrayList<String> otherPlayersPosition; /**This ArrayList contains the IDs of the square where other players are on*/
+    protected HashMap<String, Boolean> charge;
+
+    public View(Client client){
+        this.weapons = new ArrayList<>(4);
+        while (weapons.size() < 4)
+            weapons.add(null);
+
+        this.blueAmmo = this.redAmmo = this.yellowAmmo = 1;
+        this.damageBar = new ArrayList<>();
+        while (damageBar.size()<12)
+            damageBar.add(Colors.NULL);
+
+        this.marks = new ArrayList<>();
+        while (marks.size()<12)
+            marks.add(Colors.NULL);
+
+        this.powerUps = new ArrayList<>();
+        this.maxReward = 0;
+        this.otherPlayersPosition = new ArrayList<>();
+
+        this.client = client;
+    }
+
+    public abstract void showReachableSquares(List<String> squares);
+
+    public abstract void showPossibleTarget(List<String> targets);
 
     public abstract void showMessage(String message);
 
-    public void setDamageBar(List<Colors> damageBar) {}
+    public abstract GameSettingsResponse showGameSettingsRequest();
 
-    public void setMyPositionID(String myPositionID) {}
+    /**
+     * This method add a weapon to the player hand
+     * @param cardWeapon the weapon
+     * @param position the arrayList position
+     */
+    public void addWeapon(CardWeapon cardWeapon, int position){
+        weapons.set(position, cardWeapon);
+    }
+
+    /**
+     * This method is used to set the new damageBar received from server.
+     * @param damageBar an ArrayList conayining the new damageBar
+     */
+    public void setDamageBar(List<Colors> damageBar) {
+        this.damageBar = new ArrayList<>(damageBar);
+    }
+
+    public void setMyPositionID(String myPositionID) {
+        this.myPositionID = myPositionID;
+    }
+
+    public void setBlueAmmo(int blueAmmo) {
+        this.blueAmmo = blueAmmo;
+    }
+
+    public void setRedAmmo(int redAmmo) {
+        this.redAmmo = redAmmo;
+    }
+
+    public void setYellowAmmo(int yellowAmmo) {
+        this.yellowAmmo = yellowAmmo;
+    }
 }
