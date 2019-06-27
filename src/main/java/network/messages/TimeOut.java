b9eeeca52b022ce125613f364b3c19810d6580b5
Marco <MarcoPianta@users.noreply.github.com>
2019-06-27 13:12:36 +0200

    Solved conflict

---
+++ src/main/java/network/messages/TimeOut.java
@@ -0,0 +1,9 @@
+package network.messages;
+
+public class TimeOut extends Message {
+
+    public TimeOut(Integer token){
+        this.token = token;
+        this.actionType = ActionType.TIMEOUT;
+    }
+}
