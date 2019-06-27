866df23124bb2ce8c16d3abe19510e6a449466cd
Marco <MarcoPianta@users.noreply.github.com>
2019-06-27 12:09:31 +0200

    Solve conflict

--- src/main/java/network/messages/ActionType.java
+++ src/main/java/network/messages/ActionType.java
@@ -36,7 +36,8 @@ public enum ActionType {
     GRABWEAPONREQUEST("Grab weapon request"),
     DISCONNECT("Disconnect"),
     PAYMENT("payment"),
-    PAYMENTRESPONSE("Payment Response");
+    PAYMENTRESPONSE("Payment Response"),
+    MESSAGE("Message");
 
 
     private String abbreviation;
