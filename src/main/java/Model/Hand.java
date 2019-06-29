c6395dc3ec3ae7714cab49017671191930bd3d10
Marco <MarcoPianta@users.noreply.github.com>
2019-06-29 16:02:52 +0200

    Fix serializable

--- src/main/java/Model/Hand.java
+++ src/main/java/Model/Hand.java
@@ -1,9 +1,10 @@
 package Model;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Hand {
+public class Hand implements Serializable {
     private ArrayList<CardWeapon> playerWeapons;
     private ArrayList<CardPowerUp> playerPowerUps;
     private int[] ammoRYB;
